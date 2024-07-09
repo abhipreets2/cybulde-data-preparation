4b106f7acd6df874ff923db3e025803be155fe3a
abhipreets2 <abhipreets2@gmail.com>
2024-07-09 20:59:19 +0530

    downgraded pygit2 to solve dvc issue

--- cybulde/process_data.py
+++ cybulde/process_data.py
@@ -21,6 +21,7 @@ def process_data(config) -> None:
     if (config.fetch_data):
         logger.info("Fetching data...")
         get_raw_data_with_version(version=config.version, data_local_save_dir=config.data_local_save_dir, dvc_remote_repo=config.dvc_remote_repo, dvc_data_folder=config.dvc_data_folder, github_user_name=config.github_user_name, github_access_token=access_secret_version(config.infrastructure.project_id, config.github_access_token_secret_id))
+        logger.info("Data stored in local.")
     exit(0)
     cluster = instantiate(config.dask_cluster) 
     client = Client(cluster)

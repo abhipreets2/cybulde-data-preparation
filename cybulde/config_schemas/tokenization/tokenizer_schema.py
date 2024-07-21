bb0593373de33c330c73246a8f08ae6dcf3ff698
abhipreets2 <abhipreets2@gmail.com>
2024-07-21 19:26:30 +0530

    added decoder schema and config

--- cybulde/config_schemas/tokenization/tokenizer_schema.py
+++ cybulde/config_schemas/tokenization/tokenizer_schema.py
@@ -8,6 +8,7 @@ from cybulde.config_schemas.tokenization import pre_tokenizer_schema
 from cybulde.config_schemas.tokenization import model_schema
 from cybulde.config_schemas.tokenization import trainer_schema
 from cybulde.config_schemas.tokenization import normalizer_schema
+from cybulde.config_schemas.tokenization import decoder_schema
 
 @dataclass
 class TokenizerConfig:
@@ -20,7 +21,8 @@ class HuggingFaceTokenizerConfig(TokenizerConfig):
     model: model_schema.ModelConfig = MISSING
     trainer: trainer_schema.TrainerConfig = MISSING
     normalizer: Optional[normalizer_schema.NormalizerConfig] = None
-
+    decoder: Optional[decoder_schema.DecoderConfig] = None
+    
     unk_token: Optional[str] = "[UNK]"
     cls_token: Optional[str] = "[CLS]"
     sep_token: Optional[str] = "[SEP]"
@@ -33,6 +35,7 @@ def setup_config() -> None:
     model_schema.setup_config()
     trainer_schema.setup_config()
     normalizer_schema.setup_config()
+    decoder_schema.setup_config()
 
     cs = ConfigStore.instance()
     cs.store(

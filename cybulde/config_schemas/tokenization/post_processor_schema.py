ec8a05ab8e22910b2f39c3e431e27295f2cc9bf3
abhipreets2 <abhipreets2@gmail.com>
2024-07-21 19:34:57 +0530

    added post-processor schema and config

---
+++ cybulde/config_schemas/tokenization/post_processor_schema.py
@@ -0,0 +1,52 @@
+from pydantic.dataclasses import dataclass
+from omegaconf import MISSING
+
+from hydra.core.config_store import ConfigStore
+
+@dataclass
+class PostProcessorConfig:
+    _target_: str = MISSING
+
+
+@dataclass
+class BertPostProcessorConfig(PostProcessorConfig):
+    _target_: str = "tokenizers.processors.BertProcessing"
+    sep: tuple[str, int] = MISSING
+    cls: tuple[str, int] = MISSING
+
+
+@dataclass
+class ByteLevelPostProcessorConfig(PostProcessorConfig):
+    _target_: str = "tokenizers.processors.ByteLevel"
+    trim_offset: bool = True
+
+
+@dataclass
+class RobertaPostProcessorConfig(PostProcessorConfig):
+    _target_: str = "tokenizers.processors.RobertaProcessing"
+    sep: tuple[str, int] = MISSING
+    cls: tuple[str, int] = MISSING
+    trim_offset: bool = True
+    add_prefix_space: bool = True
+
+
+def setup_config() -> None:
+    cs = ConfigStore.instance()
+
+    cs.store(
+        group="tokenizer/post_processor",
+        name="bert_post_processing_schema",
+        node=BertPostProcessorConfig,
+    )
+
+    cs.store(
+        group="tokenizer/post_processor",
+        name="byte_level_post_processing_schema",
+        node=ByteLevelPostProcessorConfig,
+    )
+
+    cs.store(
+        group="tokenizer/post_processor",
+        name="roberta_post_processing_schema",
+        node=RobertaPostProcessorConfig,
+    )
--- cybulde/config_schemas/tokenization/tokenizer_schema.py
+++ cybulde/config_schemas/tokenization/tokenizer_schema.py
@@ -9,6 +9,7 @@ from cybulde.config_schemas.tokenization import model_schema
 from cybulde.config_schemas.tokenization import trainer_schema
 from cybulde.config_schemas.tokenization import normalizer_schema
 from cybulde.config_schemas.tokenization import decoder_schema
+from cybulde.config_schemas.tokenization import post_processor_schema
 
 @dataclass
 class TokenizerConfig:
@@ -22,6 +23,7 @@ class HuggingFaceTokenizerConfig(TokenizerConfig):
     trainer: trainer_schema.TrainerConfig = MISSING
     normalizer: Optional[normalizer_schema.NormalizerConfig] = None
     decoder: Optional[decoder_schema.DecoderConfig] = None
+    post_processor: Optional[post_processor_schema.PostProcessorConfig] = None
     
     unk_token: Optional[str] = "[UNK]"
     cls_token: Optional[str] = "[CLS]"
@@ -36,6 +38,7 @@ def setup_config() -> None:
     trainer_schema.setup_config()
     normalizer_schema.setup_config()
     decoder_schema.setup_config()
+    post_processor_schema.setup_config()
 
     cs = ConfigStore.instance()
     cs.store(
